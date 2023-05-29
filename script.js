9933df1da87d7de60e4a244d878a4f2dc3cf56f4
Ro-vallejos <roberta.vallejos@gmail.com>
2023-05-29 17:21:35 -0300

    .

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-let slide= ["img/banner1.png","img/banner2.png"];
+let slide= [
+  {
+    imagen:"img/banner1.png",
+    linkImagen:"contacto.html",
+  },
+  {
+    imagen:"img/banner2.png",
+    linkImagen:"index.html",
+  }
+];
 let pos=0;
 mostrar();
 function mostrar(){
-    document.getElementById("pic").src = slide[pos];
+    document.getElementById("pic").src = slide[pos].imagen;
+    document.getElementById("linkImg").href=slide[pos].linkImagen;
 }
 function siguiente(){
     if (pos < slide.length - 1) {
